0fc0b7c3ae0d5ebdff0ba43e0084cd6e2e09f033
Sushant0505 <sushant.8432@gmail.com>
2026-03-06 20:49:11 +0530

    app.js

--- app.js
+++ app.js
@@ -3,29 +3,29 @@ require("dotenv").config();
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
-const cookiePaser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 
 const Blog = require("./models/blog");
 
 const userRoute = require("./routes/user");
 const blogRoute = require("./routes/blog");
 
-const {
-  checkForAuthenticationCookie,
-} = require("./middlewares/authentication");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// MongoDB connection
 mongoose
-.connect(process.env.MONGO_URL)
-.then((e) => console.log("MongoDB Connected"));
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Error:", err));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
-app.use(cookiePaser());
+app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
 app.use(express.static(path.resolve("./public")));
 
